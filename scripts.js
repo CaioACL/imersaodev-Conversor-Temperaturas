be0d29ac0425bf07e860e58504e48dac7a1734dc
Caio Azevedo <87204701+CaioACL@users.noreply.github.com>
2021-07-09 16:12:30 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,14 @@
+var valCel = prompt("Qual a temperatura em Celsius?")
+
+var valFah = parseFloat(valCel)
+
+var valFah = valCel * 1.8 + 32
+var valFahfix = valFah.toFixed(1)
+
+alert(valFahfix + " Fahrenheits")
+
+//Revião
+//variáveis var int - float - string
+//alert - parseint - parseFloat - prompt
+// operações + somar * multiplicar
+//comentário final
